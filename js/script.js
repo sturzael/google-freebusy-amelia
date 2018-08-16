c406277831ed0d964276501925f700ed9b7d9cdd
sturzael <esturz23@gmail.com>
2018-08-16 15:50:36 +1200

    timeMin using current time and date

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-var APIKey, EndDate, StartDate;
+var APIKey, endDate, startDate, currentTime, isotime;
 var arr = [];
+var currentDate = new Date();
+currentDate.setMilliseconds(0);
+currentTime = currentDate.toISOString();
+isotime = currentTime.substr(0, currentTime.indexOf('.'));
 
 $.ajax({
   url: "./data.json",
@@ -23,7 +27,7 @@ function start() {
   $.ajax({
     url: 'https://www.googleapis.com/calendar/v3/freeBusy?fields=calendars%2Cgroups%2Ckind%2CtimeMax%2CtimeMin&key=' + APIKey,
     type: 'POST',
-    data: '{"apiUrl": "https://www.googleapis.com","timeMin": "2018-08-15T11:21:08+12:00","timeMax": "2018-08-17T11:21:08+12:00",  "items": [{"id":"elliot.sturzaker@nettl.com"}],"timeZone": "UTC+12:00","groupExpansionMax": 1,"calendarExpansionMax": 1}',
+    data: '{"apiUrl": "https://www.googleapis.com","timeMin": "' + isotime + '+12:00","timeMax": "2018-08-17T11:21:08+12:00",  "items": [{"id":"elliot.sturzaker@nettl.com"}],"timeZone": "UTC+12:00","groupExpansionMax": 1,"calendarExpansionMax": 1}',
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
     success: function(DataFromJson) {
@@ -39,11 +43,11 @@ function start() {
 function list(DataFromJson) {
   var busydates = DataFromJson.calendars['elliot.sturzaker@nettl.com'].busy
   for (var i = 0; i < busydates.length; i++) {
-    StartDate = busydates[i].start.substr(0, busydates[i].start.indexOf('+')).replace('T', ' ');
-    EndDate = busydates[i].end.substr(0, busydates[i].end.indexOf('+')).replace('T', ' ');
+    startDate = busydates[i].start.substr(0, busydates[i].start.indexOf('+')).replace('T', ' ');
+    endDate = busydates[i].end.substr(0, busydates[i].end.indexOf('+')).replace('T', ' ');
     arr.push({
-      'start': StartDate,
-      'end': EndDate
+      'start': startDate,
+      'end': endDate
     });
 
   }

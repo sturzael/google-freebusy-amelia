1b86179f14199668bf392ab739ef3598eb2b4dbe
sturzael <esturz23@gmail.com>
2018-08-17 11:28:41 +1200

    checking if already exists in db

--- date.php
+++ date.php
@@ -9,9 +9,13 @@ $apiArray = json_decode($myArray, true);
     foreach ($apiArray as $key => $value) {
       $start = $value['start'];
       $end = $value['end'];
-      $sql = $wpdb->insert('wp_amelia_appointments', array ('id' => NULL, 'status' => 'approved', 'bookingStart' => $start, 'bookingEnd' => $end, 'notifyParticipants' => '0', 'serviceId' => '1', 'providerId' => '1', 'internalNotes' => 'freeBusy'
-));
-
+      $count = 0;
+      $count = $wpdb->get_var("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` = '$start'  AND `bookingEnd` = '$end'");
+      if ($count < 1) {
+        $sql = $wpdb->insert('wp_amelia_appointments', array ('id' => NULL, 'status' => 'approved', 'bookingStart' => $start, 'bookingEnd' => $end, 'notifyParticipants' => '0', 'serviceId' => '1', 'providerId' => '1', 'internalNotes' => 'freeBusy'
+        ));
+      };
     }
+
 
 ?>

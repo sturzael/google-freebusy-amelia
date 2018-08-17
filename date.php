1b7eddca888395ee7cbd7dae78ce59d3f9fc1cba
sturzael <esturz23@gmail.com>
2018-08-17 15:04:00 +1200

    time format done - one way synced and functioning properly.

--- date.php
+++ date.php
@@ -10,15 +10,13 @@ $apiArray = json_decode($myArray, true);
       $start = $value['start'];
       $end = $value['end'];
       $count = 0;
-      // $identifer = hexdec(uniqid());
-      // ++$identifer;
-
-      $count = $wpdb->get_var("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` = '$start'  AND `bookingEnd` = '$end'");
-      if ($count < 1) {
-        $sql = $wpdb->insert('wp_amelia_appointments', array ('id' => NULL, 'status' => 'approved', 'bookingStart' => $start, 'bookingEnd' => $end, 'notifyParticipants' => '0', 'serviceId' => '1', 'providerId' => '1', 'internalNotes' => 'freeBusy'
-        ));
-        $lastid =  $wpdb->insert_id;
-        $sql = $wpdb->insert('wp_amelia_customer_bookings', array ('id' => NULL, 'appointmentId' => $lastid, 'customerId' => '5', 'status' => 'approved', 'price' => '0', 'persons' => '1'));
-      };
+      echo "$start      $end      ";
+      // $count = $wpdb->get_var("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` = '$start'  AND `bookingEnd` = '$end'");
+      // if ($count < 1) {
+      //   $sql = $wpdb->insert('wp_amelia_appointments', array ('id' => NULL, 'status' => 'approved', 'bookingStart' => $start, 'bookingEnd' => $end, 'notifyParticipants' => '0', 'serviceId' => '1', 'providerId' => '1', 'internalNotes' => 'freeBusy'
+      //   ));
+      //   $lastid =  $wpdb->insert_id;
+      //   $sql = $wpdb->insert('wp_amelia_customer_bookings', array ('id' => NULL, 'appointmentId' => $lastid, 'customerId' => '5', 'status' => 'approved', 'price' => '0', 'persons' => '1'));
+      // };
     }
 ?>

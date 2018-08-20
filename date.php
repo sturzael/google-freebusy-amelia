fb57b07b08f1887d18562d88b9341953417b7bea
sturzael <esturz23@gmail.com>
2018-08-20 14:30:53 +1200

    middle of sorting out delete function - trying to find a way to compare the two arrays and delete whatever doesn't match up.

--- date.php
+++ date.php
@@ -1,10 +1,25 @@
 <?php
+$items = array();
+
 define( 'SHORTINIT', true );
 require_once($_SERVER['DOCUMENT_ROOT'].'/wp-config.php'); //Calling everything needed to make the querys.
 
 global $wpdb;
+
 $myArray = $_REQUEST['jsonString']; //setting myarray variable as the dates specified in script.js
 $apiArray = json_decode($myArray, true); //decode the json into a php array
+
+
+
+$notCal = $wpdb->get_results("SELECT * FROM `wp_amelia_appointments` WHERE `internalNotes` = 'freeBusy' AND `serviceId` = 4");
+
+
+foreach ($notCal as $key => $value) {
+   $items[] = $value['bookingStart'];
+}
+// print_r($items);
+// $diff = array_diff($apiArray['start'],$items);
+// print_r($diff);
 
     foreach ($apiArray as $key => $value) { //for each item in the array run the the following
       $start = $value['start']; //setting $start as the start date specified in the array
@@ -15,11 +30,12 @@ $apiArray = json_decode($myArray, true); //decode the json into a php array
       $count = $wpdb->get_var("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` = '$start'  AND `bookingEnd` = '$end'"); //checking wether it already exists in the database
 
 
-      $notCal = $wpdb->get_col("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` <> '$start' AND `bookingEnd` <> '$end' AND `internalNotes` = 'freeBusy' AND `serviceId` = 4"); //query broken
+      // $notCal = $wpdb->get_col("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` <> '$start' AND `bookingEnd` <> '$end' AND `internalNotes` = 'freeBusy' AND `serviceId` = 4"); //query broken
 
-      foreach ($notCal as $key => $value) {
-        $sql = $wpdb->delete('wp_amelia_appointments', array ('id' => $value));
-      }
+      // foreach ($notCal as $key => $value) {
+      //   echo "- $value -";
+      //   // $sql = $wpdb->delete('wp_amelia_appointments', array ('id' => $value));
+      // }
 
 
       if ($count < 1) { //if it doesnt exist run this

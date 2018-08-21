678f07b23e8fc99b428fdda5cab91a0f91d252a6
sturzael <esturz23@gmail.com>
2018-08-22 11:18:19 +1200

    deleting and add properly - need to find a way to compare timezones and cross check

--- date.php
+++ date.php
@@ -3,61 +3,69 @@ $googleArray = array();
 $databaseArray = array();
 
 define('SHORTINIT', true);
+
 require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php'); //Calling everything needed to make the querys.
 
 global $wpdb;
+
 $myArray = $_REQUEST['jsonString']; //setting myarray variable as the dates specified in script.js
 
 $apiArray = json_decode($myArray, true); //decode the json into a php array
 
-
 $notCal = $wpdb->get_results("SELECT * FROM `wp_amelia_appointments` WHERE `internalNotes` = 'freeBusy' AND `serviceId` = 4", ARRAY_A);
 
-
-
 foreach ($notCal as $row) {
-        $databaseArray[] = $row['bookingStart'];
+  $databaseArray[] = $row['bookingStart'];
 }
 foreach ($apiArray as $rows) {
-        $googleArray[] = $rows['start'];
+  $googleArray[] = $rows['start'];
 }
 
-print_r($googleArray);
-echo "<br> <br>";
-print_r($databaseArray);
-echo "<br> <br>";
+
+
 $result = array_diff($databaseArray, $googleArray);
-print_r ($result);
+
+
+foreach ($result as $itemtoremove) {
+  $sql = $wpdb->delete('wp_amelia_appointments', array ('bookingStart' => $itemtoremove));
+}
 
 
 foreach ($apiArray as $key => $value) { //for each item in the array run the the following
+
         $start = $value['start']; //setting $start as the start date specified in the array
         $end   = $value['end']; //setting $end as the start date specified in the array
         $count = 0; //set count as 0 to be used later
+
         // $start = date('Y-m-d H:i:s', strtotime( "$start - 10 hours")); //converting the time in NZ to GMT +2 as WP_Amelia uses Serbia timezones to read it
+
         // $end = date('Y-m-d H:i:s', strtotime( "$end - 10 hours"));//converting the time in NZ to GMT +2 as WP_Amelia uses Serbia timezones to read it
+
+
         $count = $wpdb->get_var("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` = '$start'  AND `bookingEnd` = '$end'"); //checking wether it already exists in the database
+
         if ($count < 1) { //if it doesnt exist run this
-                $sql    = $wpdb->insert('wp_amelia_appointments', array(
-                        'id' => NULL,
-                        'status' => 'approved',
-                        'bookingStart' => $start,
-                        'bookingEnd' => $end,
-                        'notifyParticipants' => '0',
-                        'serviceId' => '4',
-                        'providerId' => '1',
-                        'internalNotes' => 'freeBusy'
+                $sql = $wpdb->insert('wp_amelia_appointments', array(
+                  'id' => NULL,
+                  'status' => 'approved',
+                  'bookingStart' => $start,
+                  'bookingEnd' => $end,
+                  'notifyParticipants' => '0',
+                  'serviceId' => '4',
+                  'providerId' => '1',
+                  'internalNotes' => 'freeBusy'
                 )); //insert the appointment
+
                 $lastid = $wpdb->insert_id; //grab the ID as the two tables need to be linked together
-                $sql    = $wpdb->insert('wp_amelia_customer_bookings', array(
-                        'id' => NULL,
-                        'appointmentId' => $lastid,
-                        'customerId' => '5',
-                        'status' => 'approved',
-                        'price' => '0',
-                        'persons' => '1'
+
+                $sql = $wpdb->insert('wp_amelia_customer_bookings', array(
+                  'id' => NULL,
+                  'appointmentId' => $lastid,
+                  'customerId' => '5',
+                  'status' => 'approved',
+                  'price' => '0',
+                  'persons' => '1'
                 )); //insert the front end booking and link the ID's together.
-        }
-        ;
+        };
 }
 ?>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
   </head>
   <body>
-    <h1>Only used for local tests - You need to change your locations as well.</h1>
+    <h1>Only used for local tests - You need to change your locations inside script / php files as well.</h1>
         <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
         <script type="text/javascript" src="js/script.js"></script>
   </body>

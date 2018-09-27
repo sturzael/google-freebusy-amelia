e3b58843508fadbb277b59b7fddc417371cfe744
sturzael <esturz23@gmail.com>
2018-09-28 10:18:44 +1200

    if dst else dst

--- date.php
+++ date.php
@@ -1,68 +1,72 @@
 <?php
-$googleArray = array(); //used for delete functions
+$googleArray = array();
 $databaseArray = array();
 
 date_default_timezone_set("NZ");
 
 define('SHORTINIT', true);
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php'); //Calling everything needed to make the querys.
+require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php');
 
 global $wpdb;
 
-$myArray = $_REQUEST['jsonString']; //setting myarray variable as the dates specified in script.js
+$myArray = $_REQUEST['jsonString'];
 
-$apiArray = json_decode($myArray, true); //decode the json into a php array
+$apiArray = json_decode($myArray, true);
 
-//DELETING FROM DATABASE IF IT HAS BEEN DELETED IN GOOGLE CALENDAR
 
-$notCal = $wpdb->get_results("SELECT * FROM `wp_amelia_appointments` WHERE `internalNotes` = 'freeBusy' AND `serviceId` = 4", ARRAY_A); //Get all the appointments that have been creataed by this program. CHANGE SERVICE ID TO WHATEVER SERVICE ID YOU HAVE SET BELOW
 
-foreach ($notCal as $row) { //adding only the start times from the database to an array called databaseArray
+$notCal = $wpdb->get_results("SELECT * FROM `wp_amelia_appointments` WHERE `internalNotes` = 'freeBusy' AND `serviceId` = 4", ARRAY_A);
+
+foreach ($notCal as $row) {
   $databaseArray[] = $row['bookingStart'];
 }
 
-foreach ($apiArray as $rows) { //adding only the start times from google into an array
+foreach ($apiArray as $rows) {
   $formattedItem = $rows['start'];
-  $unixTime = strtotime($formattedItem);
-
-  $formattedItem = date('Y-m-d H:i:s', strtotime( "$formattedItem - 10 hours")); //gotta convert times from google so that it mathces the time format used in the db
+  $unixTime = date('Y-m-d H:i:s',strtotime($formattedItem));
+  daylightSavings($unixTime);
+  $formattedItem = date('Y-m-d H:i:s', strtotime( "$formattedItem - 10 hours"));
   $googleArray[] =  $formattedItem;
 }
 
 
-function daylightSaving($year)
-{
-    $months = array('September');
+function daylightSavings($unixTime){
+  
+        $startDST =  date('Y-m-d H:i:s', strtotime('first sunday of April ' .  date("Y", strtotime('+1 years')))) . "\n";
+        $currentStartDST =  date('Y-m-d H:i:s', strtotime('first sunday of April ' .  date("Y"))) . "\n";
+        $endDST =  date('Y-m-d H:i:s', strtotime('last sunday of September ' .  date("Y"))) . "\n";
+        $currentEndDST =  date('Y-m-d H:i:s', strtotime('last sunday of September ' .  date("Y", strtotime('-1 years')))) . "\n";
 
-    foreach ($months as $month) {
-        echo $month . ': ' . date('Y-m-d', strtotime('last sunday of ' . $month . ' ' . $year)) . "\n";
-    }
+        if($unixTime < $startDST && $unixTime > $endDST) {
+          echo 'We are in DST';
+        }elseif ($unixTime < $endDST && $unixTime > $currentStartDST) {
+          echo 'We are not in DST';
+        }elseif ($unixTime < $currentStartDST && $unixTime > $currentEndDST) {
+          echo 'We are in DST';
+        }
 }
 
-daylightSaving($argv[1]);
+daylightSavings();
 
 
+$result = array_diff($databaseArray, $googleArray);
 
-
-$result = array_diff($databaseArray, $googleArray); //getting the differance between the two start times - this way i can see what items were in google calendar and the db but have been since deleted.
-
-foreach ($result as $itemtoremove) { //for every difference in the array do this
-  $sql = $wpdb->delete('wp_amelia_appointments', array ('bookingStart' => $itemtoremove)); //delete each row that matches the booking start
+foreach ($result as $itemtoremove) {
+  $sql = $wpdb->delete('wp_amelia_appointments', array ('bookingStart' => $itemtoremove));
 }
 
-//ADDING TO DATABASE
-foreach ($apiArray as $key => $value) { //for each item in the array run the the following
+foreach ($apiArray as $key => $value) {
 
-        $start = $value['start']; //setting $start as the start date specified in the array
-        $end   = $value['end']; //setting $end as the start date specified in the array
-        $count = 0; //set count as 0 to be used later
+        $start = $value['start'];
+        $end   = $value['end'];
+        $count = 0;
 
-        $start = date('Y-m-d H:i:s', strtotime( "$start - 12 hours")); //converting the time in NZ to GMT +2 as WP_Amelia uses Serbia timezones to read it
+        $start = date('Y-m-d H:i:s', strtotime( "$start - 12 hours")); /
 
-        $end = date('Y-m-d H:i:s', strtotime( "$end - 12 hours"));//converting the time in NZ to GMT +2 as WP_Amelia uses Serbia timezones to read it
+        $end = date('Y-m-d H:i:s', strtotime( "$end - 12 hours"));
 
-        $count = $wpdb->get_var("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` = '$start'  AND `bookingEnd` = '$end'"); //checking wether it already exists in the database
+        $count = $wpdb->get_var("SELECT * FROM `wp_amelia_appointments` WHERE `bookingStart` = '$start'  AND `bookingEnd` = '$end'");
 
         if ($count < 1) { //if it doesnt exist run this
                 // $sql = $wpdb->insert('wp_amelia_appointments', array(
